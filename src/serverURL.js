3904b2ba3aeabcc216b7c606ffba51d9359e7340
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2023-11-20 11:31:00 +0530

    show hide password feature optimisation

--- src/serverURL.js
+++ src/serverURL.js
@@ -1,6 +1,5 @@
 // const url= "https://passmanserver.onrender.com";
-// const url= "http://localhost:4000";
+const url= "http://localhost:4000";
 // const url= "https://password-manager-api-8srw.onrender.com";
-const url= "http://27.131.210.132:4000"
 
 module.exports= url;
