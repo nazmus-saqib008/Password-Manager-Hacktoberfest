bb47c20b233fbd207928366f5d24d6da1880e7ad
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2023-11-30 11:32:04 +0530

    server url updated

--- src/serverURL.js
+++ src/serverURL.js
@@ -1,5 +1,5 @@
-// const url= "https://passmanserver.onrender.com";
+const url= "https://passmanserver.onrender.com";
 // const url= "http://localhost:4000";
-const url= "https://password-manager-api-8srw.onrender.com";
+// const url= "https://password-manager-api-8srw.onrender.com";
 
 module.exports= url;
